803d511c95359598598c9d75bcbc292d28ff6c94
Lapka <Lapka@Lapka-Machine>
2017-02-03 11:13:39 +0200

    add links register and login

--- laravel/resources/views/auth/register.blade.php
+++ laravel/resources/views/auth/register.blade.php
@@ -65,6 +65,9 @@
                                 <button type="submit" class="btn btn-primary">
                                     Register
                                 </button>
+                                 <a class="btn btn-link" href="{{ url('/login') }}">
+                                   Login
+                               </a>
                             </div>
                         </div>
                     </form>
